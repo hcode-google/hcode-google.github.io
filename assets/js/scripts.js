cfee52e4794de2574b53bba897afa21943c81017
Joao Rangel <joaohcrangel@gmail.com>
2023-09-06 19:13:40 -0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector("#btn-menu")
+
+menu.addEventListener("click", () => {
+
+    console.log("Clicou")
+
+})
+
+const botaoEstouComSorte = document.querySelector(`form [type="button"]`);
+
+botaoEstouComSorte.addEventListener("click", () => {
+
+    location.href = "https://class.hcode.com.br"
+
+})
